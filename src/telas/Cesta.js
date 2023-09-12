7ef43d9ba0c0a986c89d704a7decafda28ab5f80
Joao Vitor <joaovitorvogel85@gmail.com>
2023-09-12 16:17:50 -0300

    uppercase

--- src/telas/Cesta.js
+++ src/telas/Cesta.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Image } from 'react-native'
 
+import topo from `../../assets/topo.png`
+
 export default function Cesta(){
-    return <Image source={}
-}
+    return <Image source={topo} />
+    
+} 
--- src/telas/cesta.js
+++ src/telas/cesta.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Image } from 'react-native'
 
-import topo from '../../assets/topo.png'
+import topo from `../../assets/topo.png`
 
 export default function Cesta(){
     return <Image source={topo} />
     
-} 
+}
