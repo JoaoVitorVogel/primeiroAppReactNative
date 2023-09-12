50a267deac4b84d10296a84e80aa7a67df505210
Joao Vitor <joaovitorvogel85@gmail.com>
2023-09-12 16:38:18 -0300

    add

--- src/telas/Cesta.js
+++ src/telas/Cesta.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Image } from 'react-native'
 
-import topo from `../../assets/topo.png`
+import topo from '../../assets/topo.png'
 
 export default function Cesta(){
     return <Image source={topo} />
