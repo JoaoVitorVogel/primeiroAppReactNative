4285cd51fc34f09bf60ad0c2de5ada7c349b1836
Joao Vitor <joaovitorvogel85@gmail.com>
2023-09-13 14:58:06 -0300

    add

---
+++ src/telas/Cesta/componentes/Topo.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function Topo(){
+    return <>
+        <Image source={topo} style={estilos.topo} />
+        <Texto style={estilos.titulo}>Detalhes da cesta</Texto>
+    </>
+}
